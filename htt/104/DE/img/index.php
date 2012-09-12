e7835ff0f751d00d2e2943ec6008de2b9b25f8ff
hertsch <ralf.hertsch@phpmanufaktur.de>
2012-09-12 05:09:07 +0200

    Added template /104 with sample for event microdata

---
+++ htt/104/DE/img/index.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * dbWatchSite
+ *
+ * @author Ralf Hertsch (ralf.hertsch@phpmanufaktur.de)
+ * @link http://phpmanufaktur.de
+ * @copyright 2010
+ * @license GNU GPL (http://www.gnu.org/licenses/gpl.html)
+ * @version $Id: index.php.htt 5 2010-12-16 06:06:46Z phpmanufaktur $
+ */
+
+header('Location: ../../../../../../index.php');
+
+?>
