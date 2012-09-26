c4838d6e46242b6840e66ef8421ebe354cbd574f
Ralf Hertsch <ralf.hertsch@phpmanufaktur.de>
2012-09-26 14:52:47 +0200

    added missing index.php files

--- index.php
+++ index.php
@@ -5,24 +5,25 @@
  *
  * @author Ralf Hertsch <ralf.hertsch@phpmanufaktur.de>
  * @link https://addons.phpmanufaktur.de/kitEvent
- * @copyright 2011-2012 phpManufaktur by Ralf Hertsch
- * @license http://www.gnu.org/licenses/gpl.html GNU Public License (GPL)
+ * @copyright 2011 - 2012
+ * @license MIT License (MIT) http://www.opensource.org/licenses/MIT
  */
 
 // include class.secure.php to protect this file and the whole CMS!
 if (defined('WB_PATH')) {
-  if (defined('LEPTON_VERSION')) include (WB_PATH . '/framework/class.secure.php');
+  if (defined('LEPTON_VERSION'))
+    include(WB_PATH.'/framework/class.secure.php');
 }
 else {
   $oneback = "../";
   $root = $oneback;
   $level = 1;
-  while (($level < 10) && (!file_exists($root . '/framework/class.secure.php'))) {
+  while (($level < 10) && (!file_exists($root.'/framework/class.secure.php'))) {
     $root .= $oneback;
     $level += 1;
   }
-  if (file_exists($root . '/framework/class.secure.php')) {
-    include ($root . '/framework/class.secure.php');
+  if (file_exists($root.'/framework/class.secure.php')) {
+    include($root.'/framework/class.secure.php');
   }
   else {
     trigger_error(sprintf("[ <b>%s</b> ] Can't include class.secure.php!", $_SERVER['SCRIPT_NAME']), E_USER_ERROR);
