62b26b4748041d2f59bc0df33f67429c167268d0
Ralf Hertsch <ralf.hertsch@phpmanufaktur.de>
2012-09-26 14:56:14 +0200

    changed initialisation of the Dwoo template engine

--- initialize.php
+++ initialize.php
@@ -33,33 +33,9 @@ else {
 // for extended error reporting set to true!
 if (!defined('KIT_DEBUG')) define('KIT_DEBUG', false);
 
-function kit_event_error_handler($level, $message, $file, $line) {
-	switch ($level):
-		case 1:			$type = 'E_ERROR'; break;
-		case 2:			$type = 'E_WARNING'; break;
-		case 4:			$type = 'E_PARSE'; break;
-		case 8:			$type = 'E_NOTICE'; break;
-		case 16: 		$type = 'E_CORE_ERROR';	break;
-		case 32: 		$type = 'E_CORE_WARNING';	break;
-		case 64: 		$type = 'E_COMPILE_ERROR'; break;
-		case 128:		$type = 'E_COMPILE_WARNING'; break;
-		case 256:		$type = 'E_USER_ERROR';	break;
-		case 512:		$type = 'E_USER_WARNING';	break;
-		case 1024: 	$type = 'E_USER_NOTICE'; break;
-		case 2047: 	$type = 'E_ALL'; break;
-		case 2048: 	$type = 'E_STRICT';	break;
-		default:		$type = $level;	break;
-	endswitch;
-	echo sprintf(	'<div style="margin:5px 15px;padding:10px;border:1px solid #000;color:#000;background-color:#ffd;">'.
-								'<table width="99%%"><colgroup><col width="120" /><col width="*" /></colgroup>'.
-								'<tr><td>Type</td><td style="font-weight:bold;color:red;">%s</td></tr><tr><td>Message</td><td style="color:red;">%s</td></tr>'.
-								'<tr><td style="vertical-align:top;">Line:File</td><td><b>%s</b> : <i>%s</i></td></tr></table></div>',
-								$type, $message, $line, $file);
-}
 // Prompt all errors and use own error_handler
 if (KIT_DEBUG == true) {
 	ini_set('error_reporting', E_ALL);
-	set_error_handler("kit_event_error_handler");
 }
 
 // include GENERAL language file
@@ -96,10 +72,6 @@ if (!file_exists($compiled_path)) mkdir($compiled_path, 0755, true);
 // init the template engine
 global $parser;
 if (!is_object($parser)) $parser = new Dwoo($compiled_path, $cache_path);
-
-// load extensions for the template engine
-$loader = $parser->getLoader();
-$loader->addDirectory(WB_PATH.'/modules/'.basename(dirname(__FILE__)).'/htt/plugins/');
 
 require_once(WB_PATH.'/modules/'.basename(dirname(__FILE__)).'/class.event.php');
 
