53ace68d36c527d09e43bb426f0e46ebc0233a81
hertsch <ralf.hertsch@phpmanufaktur.de>
2012-09-29 17:43:47 +0200

    Added LEPTON compatibillity file

---
+++ wb2lepton.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * kitEvent
+ *
+ * @author Ralf Hertsch <ralf.hertsch@phpmanufaktur.de>
+ * @link https://addons.phpmanufaktur.de/kitEvent
+ * @copyright 2011-2012 phpManufaktur by Ralf Hertsch
+ * @license http://www.gnu.org/licenses/gpl.html GNU Public License (GPL)
+ */
+
+if (!defined('LEPTON_PATH')) define('LEPTON_PATH', WB_PATH);
+if (!defined('LEPTON_URL')) define('LEPTON_URL', WB_URL);
