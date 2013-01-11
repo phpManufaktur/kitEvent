9717d94f610b0efe04a95266f1f228910c603296
hertsch <ralf.hertsch@phpmanufaktur.de>
2013-01-11 15:45:36 +0100

    updated and adapted initialize.php

--- initialize.php
+++ initialize.php
@@ -5,7 +5,7 @@
  *
  * @author Ralf Hertsch <ralf.hertsch@phpmanufaktur.de>
  * @link https://addons.phpmanufaktur.de/kitEvent
- * @copyright 2011 - 2012
+ * @copyright 2011 - 2013
  * @license MIT License (MIT) http://www.opensource.org/licenses/MIT
  */
 
@@ -31,12 +31,12 @@ else {
 // end include class.secure.php
 
 // for extended error reporting set to true!
-if (!defined('KIT_DEBUG')) define('KIT_DEBUG', false);
+if (!defined('KIT_DEBUG'))
+  define('KIT_DEBUG', false);
 
 // Prompt all errors and use own error_handler
-if (KIT_DEBUG == true) {
+if (KIT_DEBUG == true)
   ini_set('error_reporting', E_ALL);
-}
 
 // wb2lepton compatibility
 if (!defined('LEPTON_PATH'))
@@ -50,9 +50,8 @@ global $I18n;
 if (!is_object($I18n))
   $I18n = new LEPTON_Helper_I18n();
 
-if (file_exists(LEPTON_PATH.'/modules/'.basename(dirname(__FILE__)).'/languages/'.LANGUAGE.'.php')) {
+if (file_exists(LEPTON_PATH.'/modules/'.basename(dirname(__FILE__)).'/languages/'.LANGUAGE.'.php'))
   $I18n->addFile(LANGUAGE.'.php', LEPTON_PATH.'/modules/'.basename(dirname(__FILE__)).'/languages/');
-}
 
 // load language depending configuration file
 if (file_exists(LEPTON_PATH.'/modules/manufaktur_config/languages/'.LANGUAGE.'.cfg.php'))
@@ -60,35 +59,23 @@ if (file_exists(LEPTON_PATH.'/modules/manufaktur_config/languages/'.LANGUAGE.'.c
 else
   require_once LEPTON_PATH.'/modules/manufaktur_config/languages/EN.cfg.php';
 
+// tools library
+if (!class_exists('kitEventToolsLibrary'))
+  require_once(WB_PATH.'/modules/kit_event/class.tools.php');
+global $kitEventTools;
+if (!is_object($kitEventTools))
+  $kitEventTools = new kitEventToolsLibrary();
 
-if (!class_exists('dbconnectle')) 				require_once(WB_PATH.'/modules/dbconnect_le/include.php');
-if (!class_exists('kitEventToolsLibrary'))   	require_once(WB_PATH.'/modules/kit_event/class.tools.php');
-
-if (!class_exists('Dwoo')) {
+if (!class_exists('Dwoo'))
   require_once WB_PATH.'/modules/dwoo/include.php';
-}
-
 // set cache and compile path for the template engine
 $cache_path = WB_PATH.'/temp/cache';
-if (!file_exists($cache_path)) mkdir($cache_path, 0755, true);
+if (!file_exists($cache_path))
+  mkdir($cache_path, 0755, true);
 $compiled_path = WB_PATH.'/temp/compiled';
-if (!file_exists($compiled_path)) mkdir($compiled_path, 0755, true);
-
+if (!file_exists($compiled_path))
+  mkdir($compiled_path, 0755, true);
 // init the template engine
 global $parser;
-if (!is_object($parser)) $parser = new Dwoo($compiled_path, $cache_path);
-
-
-require_once(WB_PATH.'/modules/'.basename(dirname(__FILE__)).'/class.event.php');
-
-global $dbEvent;
-global $dbEventGroup;
-global $dbEventItem;
-global $kitEventTools;
-
-if (!is_object($dbEvent)) $dbEvent = new dbEvent();
-if (!is_object($dbEventGroup)) $dbEventGroup = new dbEventGroup();
-if (!is_object($dbEventItem)) $dbEventItem = new dbEventItem();
-if (!is_object($kitEventTools)) $kitEventTools = new kitEventToolsLibrary();
-
-?>
+if (!is_object($parser))
+  $parser = new Dwoo($compiled_path, $cache_path);
